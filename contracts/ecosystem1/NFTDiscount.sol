b602bde0c0f28905e9ad1f4b2b2d4e2200097c03
Brian Mullin <brmullin@email.msmary.edu>
2023-05-21 21:21:16 -0400

    stubbed markdown files

--- contracts/ecosystem1/NFTDiscount.sol
+++ contracts/ecosystem1/NFTDiscount.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.15;
 
 ///@title Smart Contract EcoSystem 1, Contract 1: NFT with a Merkle Tree Discount 
 ///@author Brian Mullin
