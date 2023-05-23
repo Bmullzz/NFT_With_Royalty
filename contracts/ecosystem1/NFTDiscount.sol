4568a5cb754837763288ef5991fc8a423c14b9a5
Brian Mullin <brmullin@email.msmary.edu>
2023-05-22 21:37:14 -0400

    add functionality to NFTDiscount

--- contracts/ecosystem1/NFTDiscount.sol
+++ contracts/ecosystem1/NFTDiscount.sol
@@ -11,6 +11,9 @@ contract NFTDiscount {
     /// You cannot iterate over mappings inside of Solidity.
     mapping(uint256 => address) private _owners;
 
+    /// first address is msg.sender, second address is the one being approved, and a bool to say if it is approved or not.
+    mapping(address => mapping(address => bool)) private _operators;
+
     function mint(uint256 _tokenId) external {
         require(_owners[_tokenId] == address(0), "Already minted");
         require(_tokenId < 21, "_tokenId too large");
@@ -18,8 +21,21 @@ contract NFTDiscount {
     }
 
     function ownerOf(uint256 _tokenId) external view returns (address) {
+        //
         require(_owners[_tokenId] != address(0), "No such token");
         return _owners[_tokenId];
     }
 
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+        //check if token id exists
+        require(_owners[_tokenId] != address(0), "Token does not exist");
+        require(_owners[_tokenId] == _from, "Cannot transfer from");
+        require(msg.sender == _from, "Required to be owner");
+
+        _owners[_tokenId] = _to;
+    }
+
+    function tokenURI(uint256 _tokenId) external view returns (string) {
+        
+    }
 }
